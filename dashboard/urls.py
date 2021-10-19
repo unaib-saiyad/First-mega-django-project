012e8fe66ac2ec3a134cad7d21722ef5dd37ed57
unaib-saiyad <unaibsaiyad18@gmail.com>
2021-10-19 15:20:46 +0530

    19-oct-2021 1. Dashboard templates setup

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Dashboard.as_view()),
+    path('', views.Dashboard.as_view(), name='home'),
+    path('about/', views.About.as_view(), name='about'),
+    path('contact/', views.Contact.as_view(), name='contact'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,60 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
+from login.models import Login
 
 
 class Dashboard(View):
     def post(self, request):
-        return None
+        return JsonResponse({"status": False, "Message": "wrong request!..."})
 
     def get(self, request):
-        return render(request, 'dashboard/index.html')
+        if request.user.is_authenticated:
+            user = request.user
+            if Login.objects.filter(user=user).exists():
+                login = Login.objects.filter(user=user).get()
+            else:
+                login = ""
+        else:
+            user = "anonymous-user"
+            login = ""
+        params = {"user": user, "login": login}
+        return render(request, 'dashboard/index.html', params)
+
+
+class About(View):
+    def post(self, request):
+        return JsonResponse({"status": False, "Message": "wrong request!..."})
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            user = request.user
+            if Login.objects.filter(user=user).exists():
+                login = Login.objects.filter(user=user).get()
+            else:
+                login = ""
+        else:
+            user = "anonymous-user"
+            login = ""
+        params = {"user": user, "login": login}
+        return render(request, 'dashboard/about.html', params)
+
+
+class Contact(View):
+    def post(self, request):
+        return JsonResponse({"status": False, "Message": "wrong request!..."})
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            user = request.user
+            if Login.objects.filter(user=user).exists():
+                login = Login.objects.filter(user=user).get()
+            else:
+                login = ""
+        else:
+            user = "anonymous-user"
+            login = ""
+        params = {"user": user, "login": login}
+        return render(request, 'dashboard/contact.html', params)
