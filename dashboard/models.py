acd88a4e04e8a7890ce5e8ba23f266d2650f8880
unaib-saiyad <unaibsaiyad18@gmail.com>
2021-10-20 16:04:03 +0530

    20-oct-2021 1. User query model add 2. Views made for query model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Query(models.Model):
+    username = models.CharField(max_length=200, default='')
+    fullname = models.CharField(max_length=200, default='')
+    email = models.CharField(max_length=200, default='')
+    mobile = models.CharField(max_length=200, default='')
+    city = models.CharField(max_length=200, default='')
+    state = models.CharField(max_length=100, default='')
+    zip = models.CharField(max_length=100, default='')
+    query = models.CharField(max_length=500, default='')
+
+    def __str__(self):
+        return "%s" % self.username
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import View
 from login.models import Login
+from .models import Query
 
 
 class Dashboard(View):
@@ -44,7 +45,30 @@ class About(View):
 
 class Contact(View):
     def post(self, request):
-        return JsonResponse({"status": False, "Message": "wrong request!..."})
+        if request.user.is_authenticated:
+            user = request.user
+            username = request.POST.get('username')
+            fullname = request.POST.get('fullname')
+            email = request.POST.get('email')
+            mobile = request.POST.get('mobile')
+            city = request.POST.get('city')
+            state = request.POST.get('state')
+            zip = request.POST.get('zip')
+            query = request.POST.get('query')
+            if (username and fullname and email and mobile and city and state and zip and query) == "":
+                return JsonResponse({"status": False, "Error": "Fields should not be empty!..."})
+            if username != user.username:
+                return JsonResponse({"status": False, "Error": "Incorrect username!..."})
+            Query.objects.create(username=username, fullname=fullname, email=email, mobile=mobile,
+                                 city=city, state=state, zip=zip, query=query)
+            if Login.objects.filter(user=user).exists():
+                login = Login.objects.filter(user=user).get()
+            else:
+                login = ""
+            params = {"user": user, "login": login, "messages": ["Your response have been saved Successfully..."]}
+        else:
+            return JsonResponse({"status": False, "Message": "User is not authenticated!..."})
+        return render(request, 'dashboard/contact.html', params)
 
     def get(self, request):
         if request.user.is_authenticated:
